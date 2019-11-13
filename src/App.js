159e0e1c5b2d7f9714b05369a1b5a93a5116b8b6
BILLthebuilder <billkariri@gmail.com>
2019-11-13 21:30:33 +0300

    rename imports

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 
-import Header from './components/Header';
-import Contacts from './components/Contacts';
+import Header from './components/layout/Header';
+import Contacts from './components/contacts/Contacts';
+import { Provider } from './context';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <Header branding="Contact Manager" />
-                <div className="container">
-                    <Contacts />
+            <Provider>
+                <div className="App">
+                    <Header branding="Contact Manager" />
+                    <div className="container">
+                        <Contacts />
+                    </div>
                 </div>
-            </div>
+            </Provider>
         );
     }
 }
