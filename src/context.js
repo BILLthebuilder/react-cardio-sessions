8a2b5016f43a9edc3211f299b072fe50ece2753a
BILLthebuilder <billkariri@gmail.com>
2019-11-13 21:31:08 +0300

    create a context file to hold our app state

---
+++ src/context.js
@@ -0,0 +1,47 @@
+/* eslint-disable import/prefer-default-export */
+import React, { Component } from 'react';
+
+const Context = React.createContext();
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'DELETE_CONTACT':
+            return {
+                ...state,
+                contacts: state.contacts.filter(contact => contact.id !== action.payload)
+            };
+        default:
+            return state;
+    }
+};
+export class Provider extends Component {
+    state = {
+        contacts: [
+            {
+                id: 1,
+                name: 'John Doe',
+                email: 'johndoe@email.com',
+                phone: '44443334422'
+            },
+            {
+                id: 2,
+                name: 'Karen Williams',
+                email: 'karen@email.com',
+                phone: '334556334422'
+            },
+            {
+                id: 3,
+                name: 'John Smith',
+                email: 'johnsmith@email.com',
+                phone: '87960958599'
+            }
+        ],
+        dispatch: action => this.setState(state => reducer(state, action))
+    };
+
+    render() {
+        return <Context.Provider value={this.state}>{this.props.children}</Context.Provider>;
+    }
+}
+
+export const { Consumer } = Context;
