126c7372b4dea540a7563ceef0c2edb0146e5a6e
BILLthebuilder <billkariri@gmail.com>
2019-11-18 23:17:34 +0300

    add notfound page

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            <h1 className="dispaly-4"> 404 Page Not found</h1>
+            <p className="lead"> Sorry, that Page does not exist</p>
+        </div>
+    );
+};
